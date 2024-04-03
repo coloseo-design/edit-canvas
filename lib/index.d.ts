1110847a401e697f15f069213b814efae1549a39
liao.yj <liao.yingju@cyberway.net.cn>
2024-04-03 16:43:03 +0800

    gitignore update

--- lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-import EditCanvas from './canvas';
-import Graphics from './graphics';
-import Image from './image';
-import Text from './text';
-import Graffiti from './graffiti';
-export { Graphics, Image, Text, Graffiti };
-export default EditCanvas;
